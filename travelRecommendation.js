e72c487bc44cefc6659eb2bcc42b0d3d18914ae2
Michael-s-b <michelsilva098@gmail.com>
2025-10-03 21:05:44 -0400

    implement search

--- travelRecommendation.js
+++ travelRecommendation.js
@@ -4,7 +4,37 @@ async function fetchData() {
     return data;
 }
 
-fetchData().then((data) => {
-    console.log(data);
+
+
+async function handleSearch(){
+    const searchInput = document.getElementById("searchField");
+    const text = searchInput.value.toLowerCase().trim();
+    let results = [];
+    const data = await fetchData().then((data) => {
+    return data;
 
 })
+    if (text === "beach" || text === "beaches") {
+        results = data.beaches;
+      } else if (text === "temple" || text === "temples") {
+        results = data.temples;
+      } else if (text === "country" || text === "countries") {
+        results = data.countries;
+      } else {
+        results = ["No matches found."];
+      }
+      console.log(results);
+    const resultContainer = document.getElementById("searchResult");
+    results.forEach(element => {
+        let containerElement = document.createElement("div");
+        let nameElement = document.createElement("p");
+        let descriptionElement = document.createElement("p")
+        nameElement.innerText = element.name;
+        descriptionElement.innerText = element.description;
+        containerElement.appendChild(nameElement)
+        containerElement.appendChild(descriptionElement)
+        resultContainer.appendChild(containerElement);
+    });
+}
+
+

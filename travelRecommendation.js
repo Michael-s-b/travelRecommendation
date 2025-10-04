83e60ff4af69ef8ee7c222dd849735e12692c2b4
Michael-s-b <michelsilva098@gmail.com>
2025-10-04 00:04:20 -0400

    update

--- travelRecommendation.js
+++ travelRecommendation.js
@@ -5,15 +5,14 @@ async function fetchData() {
 }
 
 
-
 async function handleSearch(){
     const searchInput = document.getElementById("searchField");
     const text = searchInput.value.toLowerCase().trim();
     let results = [];
     const data = await fetchData().then((data) => {
     return data;
-
 })
+const resultContainer = document.getElementById("searchResult");
     if (text === "beach" || text === "beaches") {
         results = data.beaches;
       } else if (text === "temple" || text === "temples") {
@@ -23,18 +22,36 @@ async function handleSearch(){
       } else {
         results = ["No matches found."];
       }
-      console.log(results);
-    const resultContainer = document.getElementById("searchResult");
-    results.forEach(element => {
-        let containerElement = document.createElement("div");
-        let nameElement = document.createElement("p");
-        let descriptionElement = document.createElement("p")
-        nameElement.innerText = element.name;
-        descriptionElement.innerText = element.description;
-        containerElement.appendChild(nameElement)
-        containerElement.appendChild(descriptionElement)
-        resultContainer.appendChild(containerElement);
+      resultContainer.replaceChildren()
+    if (results[0] == "No matches found.") {
+        window.alert("No matches found")
+    } else {
+        results.forEach(element => {
+        resultContainer.appendChild(createTravelCard(element));
     });
+    }
+}
+
+function handleReset(){
+    const searchInput = document.getElementById("searchField");
+    searchInput.value = "";
+    const resultContainer = document.getElementById("searchResult");
+    resultContainer.replaceChildren();
 }
 
 
+function createTravelCard(data){
+    let containerElement = document.createElement("div");
+    let nameElement = document.createElement("p");
+    let imageElement = document.createElement("img")
+    let descriptionElement = document.createElement("p")
+    nameElement.innerText = data.name;
+    descriptionElement.innerText = data.description;
+    imageElement.src = data.imageUrl
+    imageElement.style.width = "400px";
+    containerElement.appendChild(nameElement)
+    containerElement.appendChild(descriptionElement)
+    containerElement.appendChild(imageElement)
+
+    return containerElement;
+}
